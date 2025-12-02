0255cb5cdb7ddf824cfb9f64902ce9a3528611f7
serensz <punnza145@gmail.com>
2025-12-02 19:14:42 +0900

    add update_data + fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/DA_HP_checker/',   // 👈 ชื่อ repo ต้องตรงเป๊ะ
 })
